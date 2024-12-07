c2084b15e537b84d8975e7698368765593307458
yacine2410 <yacin.montacer@gmail.com>
2024-12-07 20:49:12 +0100

    compartmentalized code and added tasks

--- main.py
+++ main.py
@@ -5,56 +5,25 @@ import pygame
 import time
 import random 
 
-#Array for different snake speeds depending on difficulty
-snake_speed = 15
+# choose difficulty 1 to 3: Easy, Medium, Hard
+# Easy: Slow, Just Apples
+# Medium: Medium Speed, Mines + Apples 
+# Hard: Fast, Mines & Apples in tougher places
+# Diffculty: {1, 2, 3}
 
-#GUI dimensions
-window_x = 720 #1920
-window_y = 480 #1080
+def main_menu() -> int:
+    return diffculty_level
 
-#RGB colors
-black = pygame.Color(0, 0, 0)
-red = pygame.Color(255, 0, 0)
-white = pygame.Color(255, 255, 255)
-green = pygame.Color(0, 255, 0)
-blue = pygame.Color(0, 0, 255)
+def generate_mine():
+    #generates a mine according to difficulty level
+    #and snake length and position
 
-pygame.init()
-#Inialize game window
-pygame.display.set_caption("Snake Game")
-game_window = pygame.display.set_mode((window_x, window_y))
-
-#FPS controller
-fps = pygame.time.Clock()
-
-#default snake position
-snake_position = [100, 50]
-
-#first 4 blocks of snake's body
-#initial snake's position
-snake_body = [
-    [100, 50],
-    [90, 50],
-    [80, 50],
-    [70, 50]
-]
-
-# fruit position
-fruit_position = [
-    random.randrange(1, window_x//10) * 10,
-    random.randrange(1, window_y//10) * 10
-    ]
-fruit_spawn = True
-
-#default snake direction
-direction = "RIGHT"
-change_to = direction
-
-# initial score 
-score = 0
+def generate_apple():
+    #generates an apple according to difficulty level
+    #and snake length and position
 
 def show_score(choice, color, font, size):
-    #font object score_font
+    #font object score_font    
     score_font = pygame.font.SysFont(font, size)
 
     #display surface object
@@ -85,80 +54,137 @@ def game_over():
     pygame.quit() #deactivate pygame
     quit() #quit program
 
-#main function
-while True:
-    #handling key events
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                change_to = "UP"
-            if event.key == pygame.K_DOWN:
-                change_to = "DOWN"
-            if event.key == pygame.K_LEFT:
-                change_to = "LEFT"
-            if event.key == pygame.K_RIGHT:
-                change_to = "RIGHT"
 
-    #if two keys pressed simultaenously
-    #we dont want the snake to move to different directions simultaenously
+def play_time():
+    global change_to, direction, window_x, window_y, snake_body, snake_position, snake_speed, fruit_position, fruit_spawn, score
+    while True:
+        # handling key events
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    change_to = "UP"
+                if event.key == pygame.K_DOWN:
+                    change_to = "DOWN"
+                if event.key == pygame.K_LEFT:
+                    change_to = "LEFT"
+                if event.key == pygame.K_RIGHT:
+                    change_to = "RIGHT"
 
-    if change_to == "UP" and direction != "DOWN":
-        direction = "UP"
-    if change_to == "DOWN" and direction != "UP":
-        direction = "DOWN"
-    if change_to == "LEFT" and direction != "RIGHT":
-        direction = "LEFT"
-    if change_to == "RIGHT" and direction != "LEFT":
-        direction = "RIGHT"
+        # if two keys pressed simultaneously
+        # we don't want the snake to move to different directions simultaneously
+        if change_to == "UP" and direction != "DOWN":
+            direction = "UP"
+        if change_to == "DOWN" and direction != "UP":
+            direction = "DOWN"
+        if change_to == "LEFT" and direction != "RIGHT":
+            direction = "LEFT"
+        if change_to == "RIGHT" and direction != "LEFT":
+            direction = "RIGHT"
 
-    #moving the snake
-    if direction == "UP":
-        snake_position[1] -= 10 #y axis
-    if direction == "DOWN":
-        snake_position[1] += 10 #y axis
-    if direction == "LEFT":
-        snake_position[0] -= 10 #x axis
-    if direction == "RIGHT":
-        snake_position[0] += 10 #x axis
+        # moving the snake
+        if direction == "UP":
+            snake_position[1] -= 10
+        if direction == "DOWN":
+            snake_position[1] += 10
+        if direction == "LEFT":
+            snake_position[0] -= 10
+        if direction == "RIGHT":
+            snake_position[0] += 10
 
-    #growing mechanism
-    #if fruits and snake collide: score increments by 10
-    snake_body.insert(0, list(snake_position))
-    if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
-        score += 10
-        fruit_spawn = False
-    else:
-        snake_body.pop()
+        # growing mechanism
+        # if fruits and snake collide: score increments by 10
+        snake_body.insert(0, list(snake_position))
+        if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
+            score += 10
+            fruit_spawn = False
+        else:
+            snake_body.pop()
 
-    if not fruit_spawn:
-        fruit_position = [
-            random.randrange(1, window_x//10) * 10,
-            random.randrange(1 , window_y//10) * 10
+        if not fruit_spawn:
+            fruit_position = [
+                random.randrange(1, window_x // 10) * 10,
+                random.randrange(1, window_y // 10) * 10
             ]
-    fruit_spawn = True
-    game_window.fill(black)
+        fruit_spawn = True
+        game_window.fill(black)
 
+        # draw snake
+        for pos in snake_body:
+            pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
 
-    #draw snake
-    for pos in snake_body[1:]:
-        pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
-    
-    #draw fruit
-    pygame.draw.rect(game_window, white, pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
+        # draw fruit
+        pygame.draw.rect(game_window, white, pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
 
-    #game over conditions
-    if snake_position[0] < 0 or snake_position[0] > window_x - 10:
-        game_over()
-    if snake_position[1] < 0 or snake_position[1] > window_y - 10:
-        game_over()
-    for block in snake_body[1:]:
-        if snake_position[0] == block[0] and snake_position[1] == block[1]:
+        # game over conditions
+        if snake_position[0] < 0 or snake_position[0] > window_x - 10:
             game_over()
 
-    show_score(1, white, "time new roman", 20)
-    pygame.display.update()
-    fps.tick(snake_speed)
+        if snake_position[1] < 0 or snake_position[1] > window_y - 10:
+            game_over()
+
+        for block in snake_body[1:]:
+            if snake_position[0] == block[0] and snake_position[1] == block[1]:
+                game_over()
+
+        show_score(1, white, "times new roman", 20)
+        pygame.display.update()
+        fps.tick(snake_speed)
+
+if __name__ == "__main__":
+    #speed
+    snake_speed = 15
+
+    #GUI Dimensions
+    window_x = 720
+    window_y = 480
     
+    #RGB Colors
+    black = pygame.Color(0, 0, 0)
+    red = pygame.Color(255, 0, 0)
+    white = pygame.Color(255, 255, 255)
+    green = pygame.Color(0, 255, 0)
+    blue = pygame.Color(0, 0, 255)
+
+    pygame.init()
+    pygame.display.set_caption("Snake Game")
+    game_window = pygame.display.set_mode((window_x, window_y))
+    
+    #FPS controller
+    fps = pygame.time.Clock()
+    
+    #default snake position
+    snake_position = [100, 50]
+    
+    #first 4 blocks of snake's body
+    #initial snake's position
+    snake_body = [
+        [100, 50],
+        [90, 50],
+        [80, 50],
+        [70, 50]
+        ]
+        
+    # fruit position
+    fruit_position = [
+        random.randrange(1, window_x//10) * 10,
+        random.randrange(1, window_y//10) * 10
+        ]
+    fruit_spawn = True
+    
+    #default snake direction
+    direction = "RIGHT"
+    change_to = direction
+    
+    # initial score 
+    score = 0
+
+    play_time()
+
+
+
+
+            
+
 
     
 
